f86130aed5bd2214ef9bd02cf6e20fbddbdad074
Corvax <vlvete1990@yandex.ru>
2020-03-20 19:23:02 +0300

    hello world

---
+++ start/helloworld.java
@@ -0,0 +1,7 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        String name = "Vlad";
+        System.out.println("Hello World");
+        System.out.println("Hello " + name);
+    }
+}
