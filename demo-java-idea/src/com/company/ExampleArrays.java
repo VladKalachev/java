8fa14359b75e93fb1c3f380a910ef9065286c50f
Corvax <vlvete1990@yandex.ru>
2020-03-23 19:02:21 +0300

    java.util.Arrays

--- demo-java-idea/src/com/company/ExampleArrays.java
+++ demo-java-idea/src/com/company/ExampleArrays.java
@@ -1,19 +1,22 @@
 package com.company;
 
 /**
- *  Работа с массивами и строками
- *
- *  class Object
- *  == и equals()
- *  массивы
- *  String
- *  StringBuilder
- *  перегрузка методов
- *  java.util.Arrays
+ * Работа с массивами и строками
+ * <p>
+ * class Object
+ * == и equals()
+ * массивы
+ * String
+ * StringBuilder
+ * перегрузка методов
+ * java.util.Arrays
  */
 
 
 // java.lang.Object
+
+import java.util.Arrays;
+
 /**
  *  public String toString()
  *  public boolean equals(Object obj)
@@ -26,14 +29,15 @@ package com.company;
  *  public wait()
  */
 
+
 // Массивы
 // массив приметивных типов
 // массив ссылочных типов
 // многомерные массивы
 
-public class Arrays {
+public class ExampleArrays {
 
-    public static void main(String[] arrays){
+    public static void main(String[] arrays) {
 //      Object object = new Object();
 //      System.out.println(object.toString());
 //
@@ -92,28 +96,28 @@ public class Arrays {
 
 //        System.out.println(str.indexOf('b'));
 
-        String str = "abc";
-        String str2 = new String(new char[]{'a', 'b', 'c'});
-
-        System.out.println(str);
-        System.out.println(str2);
-        System.out.println(str == str2);
-        System.out.println(str.equals(str2));
-
-        System.out.println("a" + "b" + "d");
-
-        String tmp = "";
-        for(int i = 0; i < 100; i++){
-            tmp += i + "_";
-        }
-        System.out.println(tmp);
-
-        // StringBuilder
-        StringBuilder tmp2 = new StringBuilder();
-        for(int i = 0; i < 100; i++){
-            tmp2.append(i).append("_");
-        }
-        System.out.println(tmp2.toString());
+//        String str = "abc";
+//        String str2 = new String(new char[]{'a', 'b', 'c'});
+//
+//        System.out.println(str);
+//        System.out.println(str2);
+//        System.out.println(str == str2);
+//        System.out.println(str.equals(str2));
+//
+//        System.out.println("a" + "b" + "d");
+//
+//        String tmp = "";
+//        for(int i = 0; i < 100; i++){
+//            tmp += i + "_";
+//        }
+//        System.out.println(tmp);
+//
+//        // StringBuilder
+//        StringBuilder tmp2 = new StringBuilder();
+//        for(int i = 0; i < 100; i++){
+//            tmp2.append(i).append("_");
+//        }
+//        System.out.println(tmp2.toString());
 
         /**
          * Перегрузка методов
@@ -127,15 +131,18 @@ public class Arrays {
          *
          * Перегрузка (overload) != переопределение (override)
          */
+
+        int[] values = new int[]{1, 2, 3, 4, 5};
+        System.out.println(Arrays.binarySearch(values, 5));
     }
 
     private static void arrayExample() {
-        int[] anArray1= new int[10];
-        int[] anArray2= {0, 1, 2, 3};
+        int[] anArray1 = new int[10];
+        int[] anArray2 = {0, 1, 2, 3};
         MyClass[] objArray = new MyClass[5];
 
         System.out.println(objArray.toString());
-        for(int i = 0; i < objArray.length; i++){
+        for (int i = 0; i < objArray.length; i++) {
             objArray[i] = new MyClass(i);
         }
 
