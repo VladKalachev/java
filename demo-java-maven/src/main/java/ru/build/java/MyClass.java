0ca006a236325224fcf71b287afa64b1e667bea5
Corvax <vlvete1990@yandex.ru>
2020-03-28 13:56:05 +0300

    fix

---
+++ demo-java-maven/src/main/java/ru/build/java/MyClass.java
@@ -0,0 +1,13 @@
+package ru.build.java;
+
+public class MyClass {
+    public static void main(String[] args) {
+        System.out.println("Mains!");
+    }
+
+
+    private static void test() {
+        System.out.println("Hello!");
+
+    }
+}
