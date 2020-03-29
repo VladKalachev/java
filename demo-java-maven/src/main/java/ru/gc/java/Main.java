8e105ae1529fe833fc3b9f81a13909f5e052372a
Corvax <vlvete1990@yandex.ru>
2020-03-30 01:11:31 +0300

    Logger

--- demo-java-maven/src/main/java/ru/gc/java/Main.java
+++ demo-java-maven/src/main/java/ru/gc/java/Main.java
@@ -1,5 +1,8 @@
 package ru.gc.java;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * Сборка мусора и отладка
  *
@@ -14,7 +17,12 @@ package ru.gc.java;
  * Типы ссылок
  */
 public class Main {
+    private static Logger logger = Logger.getLogger(Main.class.getName());
     public static void main(String[] args) {
+        System.out.println(String.format("Out message %s number: %d", "sting", 10));
+        System.err.println("Err message");
 
+        logger.info("Server started");
+        logger.log(Level.SEVERE, "Can't open file");
     }
 }
