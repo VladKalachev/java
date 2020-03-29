647194d5cbaa5f99992b96d8a0bad7ea6ada8466
Corvax <vlvete1990@yandex.ru>
2020-03-29 21:22:24 +0300

    ArrayList

--- demo-java-maven/src/main/java/ru/build/java/MyClass.java
+++ demo-java-maven/src/main/java/ru/build/java/MyClass.java
@@ -1,13 +1,18 @@
 package ru.build.java;
 
+// import com.google.common.collection.Lists;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyClass {
     public static void main(String[] args) {
-        System.out.println("Mains!");
+//         test(Lists.asList("Sully", args).toString());
+        List<String> list = new ArrayList();
+        System.out.println("Main!");
     }
-
 
     private static void test() {
         System.out.println("Hello!");
-
     }
 }
