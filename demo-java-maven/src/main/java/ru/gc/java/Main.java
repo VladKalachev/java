e26455d4b7835e7d3128d8c7112c3c147fb651d6
Corvax <vlvete1990@yandex.ru>
2020-03-30 00:39:42 +0300

    GC

---
+++ demo-java-maven/src/main/java/ru/gc/java/Main.java
@@ -0,0 +1,20 @@
+package ru.gc.java;
+
+/**
+ * Сборка мусора и отладка
+ *
+ * Логирование
+ * Отладка
+ * Remote debug
+ * jconsole
+ * JMX, MBeans
+ * Сборка мусора
+ * Виды GC, алгоритмы GC
+ * GC roots
+ * Типы ссылок
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
