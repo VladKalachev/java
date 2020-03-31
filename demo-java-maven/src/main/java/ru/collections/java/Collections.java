2125e8edfaa8c4f8613162f4052325eda113bf90
Corvax <vlvete1990@yandex.ru>
2020-03-31 11:30:09 +0300

    ArrayList

--- demo-java-maven/src/main/java/ru/collections/java/Collections.java
+++ demo-java-maven/src/main/java/ru/collections/java/Collections.java
@@ -1,26 +1,51 @@
 package ru.collections.java;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.TreeSet;
+import java.util.*;
 
 public class Collections {
     public static void main(String[] args) {
-        Collection<String> c = new TreeSet<>();
+        Collection<String> c = new ArrayList<>();
         c.add("First");
         c.add("Second");
         System.out.println(c.size());
         //c.remove("First");
         //System.out.println(c.size());
         Iterator<String> iterator = c.iterator();
-        while (iterator.hasNext()){
-           String next = iterator.next();
-           System.out.println(next);
+//        while (iterator.hasNext()){
+//           String next = iterator.next();
+//           System.out.println(next);
+//        }
+//        for(String s : c) {
+//            System.out.println(s);
+//        }
+        System.out.println(c.contains("First"));
+
+        List<String> list = (List<String>) c;
+        list.add(0, "Very first");
+
+        print(list);
+
+        System.out.println(list.indexOf("First"));
+
+        Collection<String> c2 = Arrays.asList(new String[]{ "First", "Second" });
+        c.removeAll(c2);
+        print(c2);
+
+        Iterator<String> iterator2 = c.iterator();
+
+        while (iterator2.hasNext()) {
+            String next = iterator2.next();
+            if(next.equals("First")){
+                iterator2.remove();
+            }
         }
-        for(String s : c) {
+    }
+
+    private static void print(Collection<?> list) {
+        for(Object s : list) {
             System.out.println(s);
         }
-        System.out.println(c.contains("First"));
+        System.out.println();
+        System.out.println();
     }
 }
