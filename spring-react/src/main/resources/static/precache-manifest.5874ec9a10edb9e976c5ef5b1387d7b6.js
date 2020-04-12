5cb87d594d41edd5e9e9dbb841ace6b938038ba5
Corvax <vlvete1990@yandex.ru>
2020-04-12 20:24:07 +0300

    addin build frontend

---
+++ spring-react/src/main/resources/static/precache-manifest.5874ec9a10edb9e976c5ef5b1387d7b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2784e5504bc092140a2843c173f50b02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5bcf05ed2460da849a3e",
+    "url": "/static/css/2.d788185a.chunk.css"
+  },
+  {
+    "revision": "8797acc38678c100126b",
+    "url": "/static/css/main.1003e3a1.chunk.css"
+  },
+  {
+    "revision": "5bcf05ed2460da849a3e",
+    "url": "/static/js/2.9c7fb23d.chunk.js"
+  },
+  {
+    "revision": "e2efa734baa9e5c3b8ef165e6121d69e",
+    "url": "/static/js/2.9c7fb23d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8797acc38678c100126b",
+    "url": "/static/js/main.7d229c37.chunk.js"
+  },
+  {
+    "revision": "b05506d192fa13a4b14c",
+    "url": "/static/js/runtime-main.cb08f1e8.js"
+  }
+]);
