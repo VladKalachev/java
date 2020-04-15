88081b6350cb91863b038a1e8adcb09a0da8652d
Corvax <vlvete1990@yandex.ru>
2020-04-15 21:48:32 +0300

    adding hibernate

--- TodoList/src/main/java/itvdn/todolist/controllers/UserController.java
+++ TodoList/src/main/java/itvdn/todolist/controllers/UserController.java
@@ -8,8 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @Controller
 public class UserController {
 
@@ -18,43 +16,6 @@ public class UserController {
     @Autowired
     public UserController(IUserService userService) {
         this.userService = userService;
-    }
-
-    @GetMapping(path = "/create_table")
-    public ResponseEntity<String> getTest() {
-        userService.createTableUser();
-        return new ResponseEntity<>("Table has been created", HttpStatus.OK);
-    }
-
-    @PostMapping(path = "/user/create")
-    public ResponseEntity<Integer> createUser(@RequestBody User user) {
-        int result = userService.createUser(user);
-        return new ResponseEntity<>(result, HttpStatus.CREATED);
-    }
-
-    @GetMapping(path = "user/getAll")
-    public List<User> getUserAll() {
-        List<User> result = userService.getUserAll();
-        System.out.println(result);
-        return userService.getUserAll();
-    }
-
-    @GetMapping(path = "/user/get/{id}")
-    public ResponseEntity<User> getUser(@PathVariable long id) {
-        User result = userService.getUser(id);
-        return new ResponseEntity<>(result, HttpStatus.OK);
-    }
-
-    @PutMapping(path = "/user/update/{id}")
-    public ResponseEntity<Integer> updateUser(@RequestBody User updatedUser, @PathVariable long id) {
-        int result = userService.updateUser(updatedUser, id);
-        return new ResponseEntity<>(result, HttpStatus.OK);
-    }
-
-    @DeleteMapping(path = "/user/delete/{id}")
-    public ResponseEntity<Integer> deleteUser(@PathVariable long id) {
-        int result = userService.deleteUser(id);
-        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
 }
--- TodoList/src/main/java/itvdn/todolist/services/UserService.java
+++ TodoList/src/main/java/itvdn/todolist/services/UserService.java
@@ -12,68 +12,29 @@ import java.util.List;
 @Service
 public class UserService implements IUserService {
 
-    private final JdbcTemplate jdbcTemplate;
-
     @Autowired
-    public UserService(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
-
-    public void createTableUser() {
-        jdbcTemplate.execute("DROP TABLE IF EXISTS Users");
-        jdbcTemplate.execute("CREATE TABLE Users(Id LONG, Email VARCHAR(30), Password VARCHAR(30))");
-    }
-
-//    @Override
-//    public List<User> getUserAll() {
-//        String sql = "SELECT * FROM Users";
-//        return jdbcTemplate.query(
-//                sql,
-//                (rs, rowNum) ->
-//                    new User(
-//                            rs.getLong("id"),
-//                            rs.getString("email"),
-//                            rs.getString("password")
-//                    )
-//            );
-//    }
-
-    @Override
-    public List<User> getUserAll() {
-        String sql = "SELECT * FROM Users";
-        return jdbcTemplate.query(
-                sql,
-                new BeanPropertyRowMapper<>(User.class)
-            );
+    public UserService() {
     }
 
     @Override
     public int createUser(User user) {
-        String query = "INSERT INTO Users VALUES(" + user.getId() + ",'" + user.getEmail() + "','" + user.getPassword() + "')";
-        int result = jdbcTemplate.update(query);
-        return result;
+        return 0;
     }
-
-
 
     @Override
     public User getUser(long id) {
-        String query = "SELECT * FROM Users WHERE Id=?";
-        User foundUser = jdbcTemplate.queryForObject(query, new Object[]{id}, new BeanPropertyRowMapper<>(User.class));
-        return foundUser;
+        return null;
     }
 
     @Override
     public int updateUser(User updatedUser, long id) {
-        String query = "UPDATE Users SET Email='" + updatedUser.getEmail() + "', password='" + updatedUser.getPassword() + "' WHERE id=" + id;
-        int result = jdbcTemplate.update(query);
-        return result;
+
+        return 0;
     }
 
     @Override
     public int deleteUser(long id) {
-        String query = "DELETE FROM Users WHERE Id=" + id;
-        int result = jdbcTemplate.update(query);
-        return result;
+
+        return 0;
     }
 }
