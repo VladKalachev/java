79926252d51ca2303adbb588838740fcfa422bee
Corvax <vlvete1990@yandex.ru>
2020-04-29 18:14:35 +0300

    addin arrays folder

---
+++ itvdn-java-starte/src/com/itvdn/javaStarter/arrays/C04_ArraysNull.java
@@ -0,0 +1,14 @@
+package com.itvdn.javaStarter.arrays;
+
+public class C04_ArraysNull {
+    // Массивы (одномерный массив).
+
+    public static void main(String[] args) {
+        int[] array = new int[5];
+
+        // Вывод на экран значений элементов массива.
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
---
+++ itvdn-java-starte/src/com/itvdn/javaStarter/arrays/C13_MethodWithArray.java
@@ -0,0 +1,20 @@
+package com.itvdn.javaStarter.arrays;
+
+public class C13_MethodWithArray {
+    static int[] modifyArray(int[] array, int modifier) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = array[i] * modifier;
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        int[] myArray = {1, 2, 3, 4, 5};
+
+        myArray = modifyArray(myArray, 5);
+
+        for (int i = 0; i < myArray.length; i++) {
+            System.out.print(myArray[i] + ", ");
+        }
+    }
+}
